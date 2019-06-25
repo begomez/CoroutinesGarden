0218a61873925eb2230ed324283328624acbe5cb
Bernat Gómez <bernat.gomez@gmail.com>
2019-06-25 12:19:13 +0200

    Minor changes in view model

--- app/src/main/java/com/example/coroutines/presentation/viewmodel/MainFragmentViewModel.kt
+++ app/src/main/java/com/example/coroutines/presentation/viewmodel/MainFragmentViewModel.kt
@@ -8,6 +8,7 @@ import com.example.coroutines.repository.impl.PicsRepositoryImpl
 import com.example.coroutines.repository.interfaces.PicsRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 /**
@@ -33,21 +34,34 @@ class MainFragmentViewModel(var repo : PicsRepository = PicsRepositoryImpl()) : 
     fun getList() {
 
         /**
-         * "launch()" is a hook for suspend functions. Receives executor as param
+         * "launch()" is a hook for suspend functions. May receive executor as param
+         * or specify executors using "withContext()"
          *
          */
-        this.viewModelScope.launch(Dispatchers.IO) {
-            var retrievedList : List<PicData> = repo.retrieveList()
+        this.viewModelScope.launch {
+            var retrievedList: List<PicData>? = null
 
-            //XXX: post data and notify observers
-            privateData.postValue(retrievedList.map { PicView(it.id, it.urls.regular) })
+        // 1. fetch result...
+
+            //XXX: swith to back thread...
+            withContext(Dispatchers.IO) {
+                retrievedList = repo.retrieveList()
+            }
+
+
+        // 2. ... and send result to UI thread
+
+            //XXX: not needed, it is redundant, already on main thread
+            withContext(Dispatchers.Main) {
+                privateData.value = retrievedList?.map { PicView(it.id, it.urls.regular) }
+            }
         }
     }
 }
 
 
 /**
- * Factory for viewmodel that takes in some params and returns customized viewmodel
+ * Factory for viewmodels that takes in some params and returns customized viewmodel
  */
 class MainFragmentViewModelFactory(var repo : PicsRepository) : ViewModelProvider.NewInstanceFactory() {
 
